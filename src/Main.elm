a8a338395f7b0dc032d9f6ddbf558fe8f883dac8
dnlckstn434343434343 <103686906+dnlckstn434343434343@users.noreply.github.com>
2023-07-15 15:28:19 +0200

    43

--- src/Main.elm
+++ src/Main.elm
@@ -39,42 +39,8 @@ type Msg
 view : Model -> Html Msg
 view model =
     div []
-        [ a [ href "/index.html" ]
-            [ img [ class "img", src "./Bilder/Logo.jpg", alt "Logo" ] [] ]
-        , div [] []
-        , div [ class "header-links" ]
-            [ a [ href "/index.html" ] [ text "Startseite" ]
-            , a [ href "/lieblingsrezepte.html" ] [ text "Lieblingsrezepte" ]
-            , a [ href "/einkauflisten.html" ] [ text "Einkauflisten" ]
-            ]
-        , div [] []
-        , h2 [] [ text "Was kochst du heute? Klicke auf eine beliebige Kategorie und finde es heraus." ]
-        , div [ class "svg-container" ]
-            [ div []
-                [ img
-                    [ class "svg"
-                    , src "./SVGs/breakfast.svg"
-                    ]
-                    []
-                , div [class "svg.Unterschrift"] [ text "Frühstück" ]
-                ]
-            , div []
-                [ img
-                    [ class "svg"
-                    , src "./SVGs/lunch.svg"
-                    ]
-                    []
-                , div [class "svg.Unterschrift"] [ text "Mittag-/Abendessen" ]
-                ]
-            , div []
-                [ img
-                    [ class "svg"
-                    , src "./SVGs/dessert.svg"
-                    ]
-                    []
-                , div [class "svg.Unterschrift"] [ text "Dessert/Süßes" ]
-                ]
-            ]
+        [ h2 [] [ text "Was kochst du heute? Klicke auf eine beliebige Kategorie und finde es heraus." ]
+        
         ]
 
 
