7989201a436b6b1c412ea73510c214508c89175a
dnlckstn434343434343 <103686906+dnlckstn434343434343@users.noreply.github.com>
2023-07-15 15:58:12 +0200

    43

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,16 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Browser
+import Html.Events exposing (onClick)
+
+type alias Model =
+    {
+    }
+
+type Msg
+    = SomeMessage
+    | AnotherMessage String
+    | SvgClicked Int
 
 main : Program () Model Msg
 main =
@@ -17,22 +27,50 @@ init =
     { -- Initialisieren Sie hier Ihr Modell
     }
 
-type alias Model =
-    {
-    }
-
-type Msg
-    = SomeMessage
-    | AnotherMessage String
-
 view : Model -> Html Msg
 view model =
     div []
         [ a [ href "/Startseite.html" ]
             [ img [ src "./Bilder/Logo.jpg", alt "Logo" ] [] ]
         , h1 [] [ text "Was kochst du heute? Klicke auf eine beliebige Kategorie und finde es heraus." ]
+        , div [] []
+        , div [ class "header-links" ]
+            [ a [ href "/index.html" ] [ text "Startseite" ]
+            , a [ href "/lieblingsrezepte.html" ] [ text "Lieblingsrezepte" ]
+            , a [ href "/einkauflisten.html" ] [ text "Einkauflisten" ]
+            ]
+        , div [] []
+        , h2 [] [ text "Was kochst du heute? Klicke auf eine beliebige Kategorie und finde es heraus." ]
+        , div [ class "svg-container" ]
+            [ div []
+                [ img
+                    [ class "svg"
+                    , src "./SVGs/breakfast.svg"
+                    , onClick (SvgClicked 1)
+                    ]
+                    []
+                , div [class "svg.Unterschrift"] [ text "Frühstück" ]
+                ]
+            , div []
+                [ img
+                    [ class "svg"
+                    , src "./SVGs/lunch.svg"
+                    , onClick (SvgClicked 2)
+                    ]
+                    []
+                , div [class "svg.Unterschrift"] [ text "Mittag-/Abendessen" ]
+                ]
+            , div []
+                [ img
+                    [ class "svg"
+                    , src "./SVGs/dessert.svg"
+                    , onClick (SvgClicked 3)
+                    ]
+                    []
+                , div [class "svg.Unterschrift"] [ text "Dessert/Süßes" ]
+                ]
+            ]
         ]
-
 
 update : Msg -> Model -> Model
 update msg model =
@@ -45,3 +83,6 @@ update msg model =
             -- Aktualisieren Sie hier Ihr Modell basierend auf der Nachricht AnotherMessage
             model
 
+        SvgClicked int ->
+            -- Aktualisieren Sie hier Ihr Modell basierend auf der Nachricht SvgClicked
+            model
